939340090b0266dfa0583815486081a0ceec3527
Shea Smith <51303984+SheaSmith@users.noreply.github.com>
2022-08-04 12:01:14 +1200

    Further improvements to the search indexing and scraping

---
+++ app/src/main/java/com/example/cosc345project/settings/IndexSettingsSerializer.kt
@@ -0,0 +1,33 @@
+package com.example.cosc345project.settings
+
+import android.content.Context
+import androidx.datastore.core.CorruptionException
+import androidx.datastore.core.DataStore
+import androidx.datastore.core.Serializer
+import androidx.datastore.dataStore
+import com.example.cosc345project.proto.IndexSettings
+import com.google.protobuf.InvalidProtocolBufferException
+import java.io.InputStream
+import java.io.OutputStream
+
+// Suppress warnings for coroutine stuff. Bug with the interactions between protoc and the datastore library.
+@Suppress("BlockingMethodInNonBlockingContext")
+object IndexSettingsSerializer : Serializer<IndexSettings> {
+    override val defaultValue: IndexSettings
+        get() = IndexSettings.getDefaultInstance()
+
+    override suspend fun readFrom(input: InputStream): IndexSettings {
+        try {
+            return IndexSettings.parseFrom(input)
+        } catch (exception: InvalidProtocolBufferException) {
+            throw CorruptionException("Cannot read proto", exception)
+        }
+    }
+
+    override suspend fun writeTo(t: IndexSettings, output: OutputStream) = t.writeTo(output)
+}
+
+val Context.indexSettingsDataStore: DataStore<IndexSettings> by dataStore(
+    fileName = "indexSettings.pb",
+    serializer = IndexSettingsSerializer
+)
---
+++ app/src/main/java/com/example/cosc345project/workers/IndexingWorker.kt
@@ -0,0 +1,23 @@
+package com.example.cosc345project.workers
+
+import android.content.Context
+import androidx.hilt.work.HiltWorker
+import androidx.work.CoroutineWorker
+import androidx.work.WorkerParameters
+import com.example.cosc345project.repository.SearchRepository
+import dagger.assisted.Assisted
+import dagger.assisted.AssistedInject
+
+@HiltWorker
+class IndexingWorker @AssistedInject constructor(
+    @Assisted val appContext: Context,
+    @Assisted params: WorkerParameters,
+    private val searchRepository: SearchRepository
+) : CoroutineWorker(appContext, params) {
+
+    override suspend fun doWork(): Result {
+        searchRepository.indexFromFirebase(appContext)
+
+        return Result.success()
+    }
+}
