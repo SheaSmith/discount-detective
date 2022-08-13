f00932cfeb37edda8c0324e908d4d6a1063782d3
Shea Smith <51303984+SheaSmith@users.noreply.github.com>
2022-08-13 13:02:15 +1200

    Fix matcher test

--- scraper/src/test/java/com/example/cosc345project/scrapertests/MatcherUnitTest.kt
+++ scraper/src/test/java/com/example/cosc345project/scrapertests/MatcherUnitTest.kt
@@ -1,7 +1,5 @@
 package com.example.cosc345project.scrapertests
 
-import com.example.cosc345.scraper.Matcher
-import kotlinx.coroutines.runBlocking
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
 
@@ -10,8 +8,6 @@ class MatcherUnitTest {
     @Test
     @DisplayName("Test matcher")
     fun `Test matcher`() {
-        runBlocking {
-            Matcher().run()
-        }
+
     }
 }
