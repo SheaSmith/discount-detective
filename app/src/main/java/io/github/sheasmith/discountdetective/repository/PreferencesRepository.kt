fac12cec91fc0eecb0f09679ea99985b1e505bb2
Petra Fisher <fispe051@student.otago.ac.nz>
2022-09-22 00:31:04 +1200

    Adding documentation in response to the Codacy report.

--- app/src/main/java/io/github/sheasmith/discountdetective/repository/PreferencesRepository.kt
+++ app/src/main/java/io/github/sheasmith/discountdetective/repository/PreferencesRepository.kt
@@ -18,11 +18,21 @@ import javax.inject.Singleton
 class PreferencesRepository @Inject constructor(@ApplicationContext context: Context) {
     private val sharedPreferences = PreferenceManager.getDefaultSharedPreferences(context)
 
+    /**
+     * Gets the region preference value that the user has set using the live data class.
+     *
+     * @return A live data string of the current region.
+     */
     fun getRegion(): LiveData<String> {
 
         return sharedPreferences.stringLiveData("city", Region.DUNEDIN)
     }
 
+    /**
+     * Gets the dark/light mode preference value that the user has set using the live data class.
+     *
+     * @return A live data string of the current dark/light mode.
+     */
     fun getDarkMode(): LiveData<String> {
         return sharedPreferences.stringLiveData("dark_mode", "system")
     }
--- app/src/main/java/io/github/sheasmith/discountdetective/settings/PreferencesLiveData.kt
+++ app/src/main/java/io/github/sheasmith/discountdetective/settings/PreferencesLiveData.kt
@@ -24,6 +24,11 @@ abstract class SharedPreferenceLiveData<T>(
             }
         }
 
+    /**
+     * Retrieves the current value of the preference for which the key refers to.
+     *
+     * @return The value, T.
+     */
     abstract fun getValueFromPreferences(key: String, defValue: T): T
 
     override fun onActive() {
@@ -38,12 +43,22 @@ abstract class SharedPreferenceLiveData<T>(
     }
 }
 
+/**
+ * Shared preferences class for when the preference values are of type int.
+ *
+ * @return Shared preference live data with values of type int.
+ */
 class SharedPreferenceIntLiveData(sharedPrefs: SharedPreferences, key: String, defValue: Int) :
     SharedPreferenceLiveData<Int>(sharedPrefs, key, defValue) {
     override fun getValueFromPreferences(key: String, defValue: Int): Int =
         sharedPrefs.getInt(key, defValue)
 }
 
+/**
+ * Shared preferences class for when the preference values are of type String.
+ *
+ * @return Shared preference live data with values of type String.
+ */
 class SharedPreferenceStringLiveData(
     sharedPrefs: SharedPreferences,
     key: String,
@@ -54,6 +69,11 @@ class SharedPreferenceStringLiveData(
         sharedPrefs.getString(key, defValue)!!
 }
 
+/**
+ * Shared preferences class for when the preference values are of type Boolean.
+ *
+ * @return Shared preference live data with values of type Boolean.
+ */
 class SharedPreferenceBooleanLiveData(
     sharedPrefs: SharedPreferences,
     key: String,
@@ -64,18 +84,33 @@ class SharedPreferenceBooleanLiveData(
         sharedPrefs.getBoolean(key, defValue)
 }
 
+/**
+ * Shared preferences class for when the preference values are of type Float.
+ *
+ * @return Shared preference live data with values of type Float.
+ */
 class SharedPreferenceFloatLiveData(sharedPrefs: SharedPreferences, key: String, defValue: Float) :
     SharedPreferenceLiveData<Float>(sharedPrefs, key, defValue) {
     override fun getValueFromPreferences(key: String, defValue: Float): Float =
         sharedPrefs.getFloat(key, defValue)
 }
 
+/**
+ * Shared preferences class for when the preference values are of type Long.
+ *
+ * @return Shared preference live data with values of type Long.
+ */
 class SharedPreferenceLongLiveData(sharedPrefs: SharedPreferences, key: String, defValue: Long) :
     SharedPreferenceLiveData<Long>(sharedPrefs, key, defValue) {
     override fun getValueFromPreferences(key: String, defValue: Long): Long =
         sharedPrefs.getLong(key, defValue)
 }
 
+/**
+ * Shared preferences class for when the preference values that are a set of type String.
+ *
+ * @return Shared preference live data with values that are a set of type String.
+ */
 class SharedPreferenceStringSetLiveData(
     sharedPrefs: SharedPreferences,
     key: String,
@@ -86,10 +121,20 @@ class SharedPreferenceStringSetLiveData(
         sharedPrefs.getStringSet(key, defValue)!!
 }
 
+/**
+ * Function which returns live data for a value of type int.
+ *
+ * @return Shared preference live data for a value of type int.
+ */
 fun SharedPreferences.intLiveData(key: String, defValue: Int): SharedPreferenceLiveData<Int> {
     return SharedPreferenceIntLiveData(this, key, defValue)
 }
 
+/**
+ * Function which returns live data for a value of type String.
+ *
+ * @return Shared preference live data for a value of type String.
+ */
 fun SharedPreferences.stringLiveData(
     key: String,
     defValue: String
@@ -97,6 +142,11 @@ fun SharedPreferences.stringLiveData(
     return SharedPreferenceStringLiveData(this, key, defValue)
 }
 
+/**
+ * Function which returns live data for a value of type Boolean.
+ *
+ * @return Shared preference live data for a value of type Boolean.
+ */
 fun SharedPreferences.booleanLiveData(
     key: String,
     defValue: Boolean
@@ -104,14 +154,29 @@ fun SharedPreferences.booleanLiveData(
     return SharedPreferenceBooleanLiveData(this, key, defValue)
 }
 
+/**
+ * Function which returns live data for a value of type Float.
+ *
+ * @return Shared preference live data for a value of type Float.
+ */
 fun SharedPreferences.floatLiveData(key: String, defValue: Float): SharedPreferenceLiveData<Float> {
     return SharedPreferenceFloatLiveData(this, key, defValue)
 }
 
+/**
+ * Function which returns live data for a value of type Long.
+ *
+ * @return Shared preference live data for a value of type Long.
+ */
 fun SharedPreferences.longLiveData(key: String, defValue: Long): SharedPreferenceLiveData<Long> {
     return SharedPreferenceLongLiveData(this, key, defValue)
 }
 
+/**
+ * Function which returns live data for a value that is a set of type String.
+ *
+ * @return Shared preference live data for a value that is a set of type String.
+ */
 fun SharedPreferences.stringSetLiveData(
     key: String,
     defValue: Set<String>
